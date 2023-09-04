aeb1e75445c7a3cafdd036a05ec41a0db36a5f3e
killrpenguin <dmcfarland8031@gmail.com>
2023-09-03 22:41:03 -0600

    This repo will catch any random single page scraper projects I work on.

--- scryfall_api.py
+++ scryfall_api.py
@@ -3,9 +3,12 @@ from bs4 import BeautifulSoup
 import re
 import json
 
-link = "https://api.scryfall.com/cards/named?exact=" + "Mana Crypt"
+link = "https://api.scryfall.com/cards/named?fuzzy=" + "Valki, God of Lies // Tibalt, Cosmic Impostor"
 page = requests.get(link)
 soup = BeautifulSoup(page.text, "html.parser")
 card_dict = json.loads(soup.text)
 name = card_dict['name']
-print(name)
+print(card_dict['layout'])
+print(f"Front: {card_dict['card_faces'][0]['name']} \nBack: {card_dict['card_faces'][1]['name']}")
+if card_dict['layout'] == 'modal_dfc':
+    print(type(name))
