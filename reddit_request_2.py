1620eb81c2ccfd013e3a061b2dd742ae6e46f6a3
killrpenguin <dmcfarland8031@gmail.com>
2023-10-21 13:34:01 -0600

    This repo will catch any random single page scraper projects I work on.

--- reddit_request_2.py
+++ reddit_request_2.py
@@ -21,24 +21,36 @@ class Driver_Adapter(webdriver.Edge):
         edge_options.page_load_strategy = "eager"
         edge_options.add_argument("disable-gpu")
         # edge_options.add_argument("headless")
-        # edge_options.add_argument("--proxy_server=%s" % "http://24.158.29.166:80")
+        edge_options.add_argument("--proxy_server=%s" % "http://24.158.29.166:80")
         super().__init__(edge_options=edge_options)
         self.link = link
         self.valid_resps = [200, 301, 302, 307, 404]
-        self.sub_catagories = self.page_test()
+        self.get(self.link)
+        self.h2 = self.get_h2
+        self.sub_catagories = self.get_data
 
-    def page_test(self) -> dict:
+    @property
+    def get_h2(self) -> str:
         h2_xpath = '//*[@id="nav"]/ul/li[1]/h2'
+        wait = WebDriverWait(self, 30)
+        self.h2 = wait.until(ec.presence_of_element_located((By.XPATH, h2_xpath))).text
+        return self.h2
+
+    @property
+    def get_data(self) -> dict:
         sub_catagories_xpath = '//*[@id="nav"]/ul/li[1]/ul/descendant::a'
         wait = WebDriverWait(self, 30)
         catagories_links = wait.until(ec.presence_of_all_elements_located((By.XPATH, sub_catagories_xpath)))
-        catagories_links = {attribute.text:attribute.get_attribute("href") for attribute in catagories_links}
-        for catagories_links.keys in catagories_links:
-            print(catagories_links)
-        return catagories_links
+        self.sub_catagories = {attribute.text: attribute.get_attribute("href") for attribute in catagories_links}
+        return self.sub_catagories
 
 
-page_link = "https://www.johnlewis.com/christmas/c5000025"
-
-pages = Driver_Adapter(link=page_link)
-time.sleep(1)
+with open("pages", "r") as all_links:
+    with open("scraped_data", "x") as scraped_data:
+        page_links = all_links.read().strip().split('\n')
+        for page_link in page_links:
+            scraper = Driver_Adapter(link=page_link)
+            scraped_data.write(f"{scraper.h2}\n")
+            for (key, value) in zip(scraper.sub_catagories.keys(), scraper.sub_catagories.values()):
+                scraped_data.write(f'\t{key},{value}\n')
+            scraped_data.write(f'\n')
